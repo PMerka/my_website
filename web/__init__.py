39441827125f2d9d4b24070f85ab0735dbd2d813
merkap <pavel@merka.eu>
2021-10-14 14:51:08 +0200

    this is first test of web_app

---
+++ web/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY']="SECRET_KEY"
+    from .views import views
+    app.register_blueprint(views)
+
+    return app
